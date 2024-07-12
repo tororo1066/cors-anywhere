cb9ead9367037690d997ee69bd29970e59ba1eec
tororo_1066 <panshoku4@gmail.com>
2024-07-12 16:57:30 +0900

    .7

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+import "https://deno.land/x/dotenv/load.ts";
+
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = Deno.env.get("HOST") || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = Deno.env.get("PORT") || 8080;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originBlacklist = parseEnvList(Deno.env.get("CORSANYWHERE_BLACKLIST"));
+var originWhitelist = parseEnvList(Deno.env.get("CORSANYWHERE_WHITELIST"));
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -17,7 +19,7 @@ function parseEnvList(env) {
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+var checkRateLimit = require('./lib/rate-limit')(Deno.env.get("CORSANYWHERE_RATELIMIT"));
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
